1d03c7ccb82c78e6c6c1ccc4a2d37c1f8449de7c
ITDQ-COM <80770480+ITDQ-COM@users.noreply.github.com>
2025-06-14 14:53:08 +0800

    Update selfdrived.py
    修改FCW触发条件

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -351,9 +351,9 @@ class SelfdriveD:
 
     # Check for FCW
     stock_long_is_braking = self.enabled and not self.CP.openpilotLongitudinalControl and CS.aEgo < -1.25
-    model_fcw = self.sm['modelV2'].meta.hardBrakePredicted and not CS.brakePressed and not stock_long_is_braking
-    planner_fcw = self.sm['longitudinalPlan'].fcw and self.enabled
-    if (planner_fcw or model_fcw) and not self.CP.notCar:
+    model_fcw = self.sm['modelV2'].meta.hardBrakePredicted
+    planner_fcw = self.sm['longitudinalPlan'].fcw
+    if (planner_fcw or model_fcw):
       self.events.add(EventName.fcw)
 
     # TODO: fix simulator
